6e8b484f6eb5299fbd5aee8fa64d7eb538805e06
Cours3r <benjamin@stewart.tc>
2018-04-14 14:18:40 -0400

    Edited lock2.sh to work with the i3lock-color update. All that was required to remove the text markup option

--- lockscreen/lock2.sh
+++ lockscreen/lock2.sh
@@ -8,5 +8,6 @@ tmpbg='/tmp/screen.png'
 scrot "$tmpbg"
 convert "$tmpbg" -scale 10% -scale 1000% "$tmpbg"
 convert "$tmpbg" "$icon" -gravity center -composite -matte "$tmpbg"
-#i3lock -u -i "$tmpbg"
-i3lock --textcolor=ffffff00 --insidecolor=ffffff10 --ringcolor=ffffff3e --linecolor=ffffff00 --keyhlcolor=00000080 --ringvercolor=00000000 --separatorcolor=22222260 --insidevercolor=0000001c --ringwrongcolor=00000055 --insidewrongcolor=0000001c -i "$tmpbg"
+#i3lock -u -e -i "$tmpbg"
+i3lock --insidecolor=ffffff10 --ringcolor=ffffff3e --linecolor=ffffff00 --keyhlcolor=00000080 --ringvercolor=00000000 --separatorcolor=22222260 --insidevercolor=0000001c --ringwrongcolor=00000055 --insidewrongcolor=0000001c -i "$tmpbg"
+
